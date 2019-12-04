acf0b258facf786f2216306e98da5e6bbd8f0738
Jeff Manassa <jeffmanassa@gmail.com>
2019-12-04 07:58:08 -0500

    Start up UI

---
+++ Project2-App/app/src/main/java/edu/fsu/cs/mobileproject2/HowToPlayERS.java
@@ -0,0 +1,14 @@
+package edu.fsu.cs.mobileproject2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HowToPlayERS extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_how_to_play_ers);
+    }
+}
---
+++ Project2-App/app/src/main/java/edu/fsu/cs/mobileproject2/SelectGameActivity.java
@@ -0,0 +1,58 @@
+package edu.fsu.cs.mobileproject2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SelectGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_game);
+
+        Button htpERS = (Button)findViewById(R.id.ers_info_button);
+        Button htpRPS = (Button)findViewById(R.id.rps_info_button);
+        Button htpSpoons = (Button)findViewById(R.id.spoons_info_button);
+        Button htpWar = (Button)findViewById(R.id.war_info_button);
+        Button htpYahtzee = (Button)findViewById(R.id.button15);
+
+        htpERS.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent(SelectGameActivity.this, HowToPlayERS.class));
+            }
+        });
+
+        htpRPS.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent(SelectGameActivity.this, HowToPlayRPS.class));
+            }
+        });
+
+        htpSpoons.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent(SelectGameActivity.this, HowToPlaySpoons.class));
+            }
+        });
+
+        htpWar.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent(SelectGameActivity.this, HowToPlayWar.class));
+            }
+        });
+
+        htpYahtzee.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent(SelectGameActivity.this, HowToPlayYahtzee.class));
+            }
+        });
+    }
+}
